61caf6f5d64297f549dde4029166dd1ad6586c91
Hubert Duchatelle <hubert.duchatelle@rte-france.com>
2025-02-18 17:23:20 +0100

    alert test

--- node-service-test/src/index.ts
+++ node-service-test/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Test")
+import express from 'express';
+
+const app = express();
+const port = 2109;
+
+app.use(express.json());
+
+app.post('/alert', (req, res) => {
+    console.log(req.body);
+    res.send();
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
