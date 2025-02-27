57f55ef3ff59fe9149b8ff54e677a3ed95e8b672
Hubert Duchatelle <hubert.duchatelle@rte-france.com>
2025-02-27 17:02:26 +0100

    first version

--- node-services/alerting-service/src/index.ts
+++ node-services/alerting-service/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import {getLogger} from './logger';
+import {sendCard} from './cardSender';
 
+const port = 2109;
 const app = express();
 const logger = getLogger();
 
@@ -11,6 +13,23 @@ app.post('/alert', (req, res) => {
     res.send();
 });
 
-const port = 2109;
 logger.info(`listening on port ${port}`);
-app.listen(port);
+//app.listen(port);
+
+const test_card = {
+    publisher: 'opfab',
+    processVersion: '2',
+    process: 'monitoringProcess',
+    processInstanceId: 'panel-1',
+    state: 'panelState',
+    groupRecipients: ['Dispatcher'],
+    severity: 'INFORMATION',
+    startDate: new Date().valueOf(),
+    summary: {key: 'monitoringProcess.summary'},
+    title: {key: 'monitoringProcess.panelTitle'},
+    data: {
+        url: 'http://localhost:3000/d-solo/ce3mo0w8g2jnkd/opfab-dashboard?from=&to=now&timezone=browser&orgId=1&panelId=2&__feature.dashboardSceneSolo'
+    }
+};
+
+sendCard(test_card);
