a24068000df9e46ad1ce8ea2ede95c292ab2aabb
Hubert Duchatelle <hubert.duchatelle@rte-france.com>
2025-03-20 10:58:23 +0100

    add mapping admin screen

--- node-services/alerting-service/src/mapping/mappingService.ts
+++ node-services/alerting-service/src/mapping/mappingService.ts
@@ -1,5 +1,5 @@
 import config from 'config';
-import {getAlertRules} from '../common/grafanaInterface';
+import {getAlertRules, getFolders} from '../common/grafanaInterface';
 import {getEntities} from '../common/opfabInterface';
 import MappingConfig from './mappingConfig';
 import MappingData from './mappingDataModel';
@@ -14,24 +14,16 @@ export default class MappingService {
     }
 
     public async getConfig(): Promise<any> {
-        let alertRules = await getAlertRules();
-        let entities = await getEntities();
-        const mappings = Object.fromEntries(this.mappingConfig.getAllMappings());
-
-        alertRules = alertRules.reduce((validRules: any[], rule: any) => {
+        const grafanaFolders = new Map((await getFolders()).map((folder) => [folder.uid, folder.title]));
+        const alertRules = (await getAlertRules()).reduce((validRules: any[], rule) => {
             if (rule.notification_settings?.receiver === config.get('grafana.contactPointName')) {
-                validRules.push({
-                    uid: rule.uid,
-                    title: rule.title,
-                    folderUid: rule.folderUID
-                });
+                const folderName = grafanaFolders.get(rule.folderUID) ?? '?';
+                validRules.push([rule.uid, folderName + '/' + rule.title]);
             }
             return validRules;
         }, []);
-        entities = entities.map((entity: any) => ({
-            id: entity.id,
-            name: entity.name
-        }));
+        const entities = (await getEntities()).map((entity: any) => [entity.id, entity.name]);
+        const mappings = Array.from(this.mappingConfig.getAllMappings());
 
         return {alertRules, entities, mappings};
     }
