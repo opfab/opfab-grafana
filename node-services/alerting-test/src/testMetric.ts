8ded9d8db8955d5519a76b5e0bfae1433869906c
Hubert Duchatelle <hubert.duchatelle@rte-france.com>
2025-04-17 17:34:57 +0200

    create demo scenario

--- node-services/alerting-test/src/testMetric.ts
+++ node-services/alerting-test/src/testMetric.ts
@@ -2,15 +2,42 @@ import express from 'express';
 import {Gauge, register} from 'prom-client';
 import readline from 'node:readline';
 
-const app = express();
 const port = 2110;
-
-const testGauge = new Gauge({
-    name: 'test_gauge',
-    help: 'Gauge metric test',
-    // labelNames: ['test_label']
+const intervalsId = new Map();
+const ranges = [
+    {start: 50, min: 49.85, max: 50.15},
+    {start: 50.35, min: 50.3, max: 50.4},
+    {start: 50.65, min: 50.6, max: 50.7},
+    {start: 49.65, min: 49.6, max: 49.7},
+    {start: 49.35, min: 49.3, max: 49.4}
+];
+const gauge = new Gauge({
+    name: 'network_frequency_hertz',
+    help: 'network frequency',
+    labelNames: ['zone']
 });
-testGauge.set(5);
+
+setRange('France', ranges[0]);
+setRange('Spain', ranges[0]);
+
+function setRange(zone: string, range: any) {
+    const intervalId = intervalsId.get(zone);
+    if (intervalId) clearInterval(intervalId);
+
+    gauge.set({zone: zone}, range.start);
+    const newIntervalId = setInterval(async () => {
+        const value = (await gauge.get()).values.find((v) => v.labels.zone === zone)?.value;
+
+        while (1) {
+            const inc = Math.random() * 0.2 - 0.1;
+            if (value && value + inc > range.min && value + inc < range.max) {
+                gauge.inc({zone: zone}, inc);
+                break;
+            }
+        }
+    }, 5000);
+    intervalsId.set(zone, newIntervalId);
+}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -18,12 +45,14 @@ const rl = readline.createInterface({
 });
 
 function getInput() {
-    rl.question('Set value for test metric (alert fires above 10): ', (value) => {
-        testGauge.set(Number(value));
+    console.log('\nSet frequency for zone France:\n1 - Normal\n2 - High\n3 - Very high\n4 - Low\n5 - Very low\n');
+    rl.question('> ', (input) => {
+        setRange('France', ranges[Number(input)-1]);
         getInput();
     });
 }
 
+const app = express();
 app.get('/metrics', async (req, res) => {
     res.set('Content-Type', register.contentType);
     res.send(await register.metrics());
